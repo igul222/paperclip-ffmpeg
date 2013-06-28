cec6bcd9a138e2fec823ff058e1b529167d05a96
Ishaan Gulrajani <igul222@gmail.com>
2013-06-28 00:07:51 -0700

    Add '-movflags faststart' option to ffmpeg call

--- lib/paperclip_processors/ffmpeg.rb
+++ lib/paperclip_processors/ffmpeg.rb
@@ -147,6 +147,8 @@ module Paperclip
       parameters << @convert_options[:output].map { |k,v| "-#{k.to_s} #{v} " if !v.nil? && (v.is_a?(Numeric) || !v.empty?) }
       parameters << "-y :dest"
 
+      parameters << "-movflags faststart"
+
       Ffmpeg.log("Building Parameters") if @whiny
       parameters = parameters.flatten.compact.join(" ").strip.squeeze(" ")
 
